fd4c09c7a868e0dbc963066c46908e3942e6ed86
guilherme683 <guilherme683@hotmail.com>
2018-06-19 20:39:25 -0300

    modified:   src/main/webapp/app/entities/scripts/scripts.component.html 	modified:   src/main/webapp/app/entities/scripts/scripts.module.ts 	modified:   src/main/webapp/app/layouts/sidebar/sidebar.component.html 	modified:   src/main/webapp/content/scss/styles.css

--- src/main/webapp/app/entities/scripts/scripts.module.ts
+++ src/main/webapp/app/entities/scripts/scripts.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../../shared/material.module';
 
 import { SicubSharedModule } from '../../shared';
 import {
@@ -23,7 +24,8 @@ const ENTITY_STATES = [
 @NgModule({
     imports: [
         SicubSharedModule,
-        RouterModule.forChild(ENTITY_STATES)
+        RouterModule.forChild(ENTITY_STATES),
+        MaterialModule
     ],
     declarations: [
         ScriptsComponent,
