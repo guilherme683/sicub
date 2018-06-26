edafa3193ca265edbaae2a3dbeef28eb557241de
guilherme683 <guilherme683@hotmail.com>
2018-06-26 18:26:29 -0300

    Revert "	modified:   src/main/webapp/app/entities/scripts/scripts-dialog.comp…"

--- src/main/webapp/app/shared/material.module.ts
+++ src/main/webapp/app/shared/material.module.ts
@@ -1,72 +1,45 @@
 import { NgModule } from '@angular/core';
 
 import {
+MatButtonModule,
+MatMenuModule,
+MatToolbarModule,
+MatIconModule,
+MatCheckboxModule,
+MatCardModule,
+MatRadioModule,
+MatDividerModule,
+MatFormFieldModule,
+MatInputModule} from '@angular/material';
+
+import { MatChipsModule } from '@angular/material/chips';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatRadioModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatInputModule,
+  ],
+  exports: [
     MatButtonModule,
     MatMenuModule,
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    MatDialogModule,
-    MatSidenavModule,
-    MatListModule,
-    MatNativeDateModule,
-    MatDatepickerModule,
-    MatSelectModule,
-} from '@angular/material';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatChipsModule } from '@angular/material/chips';
-
-@NgModule({
-    imports: [
-        MatButtonModule,
-        MatMenuModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatCardModule,
-        MatDialogModule,
-        MatInputModule,
-        MatCheckboxModule,
-        MatDividerModule,
-        MatSidenavModule,
-        MatListModule,
-        MatExpansionModule,
-        MatGridListModule,
-        MatTableModule,
-        MatSortModule,
-        MatProgressSpinnerModule,
-        MatChipsModule,
-        MatNativeDateModule,
-        MatDatepickerModule,
-        MatSelectModule
-    ],
-    exports : [
-        MatButtonModule,
-        MatMenuModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatCardModule,
-        MatDialogModule,
-        MatInputModule,
-        MatCheckboxModule,
-        MatDividerModule,
-        MatSidenavModule,
-        MatListModule,
-        MatExpansionModule,
-        MatGridListModule,
-        MatTableModule,
-        MatSortModule,
-        MatProgressSpinnerModule,
-        MatChipsModule,
-        MatNativeDateModule,
-        MatDatepickerModule,
-        MatSelectModule
-    ],
+    MatRadioModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatInputModule,
+  ]
 })
 export class MaterialModule {}
